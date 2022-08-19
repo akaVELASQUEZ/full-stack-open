06bdbf7508d303c6250a36c1edccca93a0e3e3b7
akaVELASQUEZ <allanvelasquez2397@gmail.com>
2022-08-19 12:11:01 +0800

    Exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,33 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => {
-  console.log(parts)
-  return (
-    <>
-      {parts.map((part) => {
-        return (
-          <Part key={part.id} part={part} />
-        )
-      })}
-    </>
-  )
-}
-
-
-const Course = ({course}) => {
-  console.log(course)
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-    </>
-  )
-}
+import Course from "./components/Course.js"
 
 const App = () => {
   const courses = [
@@ -74,7 +45,7 @@ const App = () => {
       ]
     }
   ]
-  
+
   return (
   <>
     {courses.map((course) => {

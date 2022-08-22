bedcef108e82d9c7529d11069fb1bfeefeb2f677
akaVELASQUEZ <allanvelasquez2397@gmail.com>
2022-08-22 14:25:19 +0800

    Exercise 2.12

---
+++ part2/countries/src/App.js
@@ -0,0 +1,79 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Filter from './components/Filter.js'
+
+const Result = ({countries, search}) => {
+    const searchResult = countries.filter((element) => element.name.common.includes(search))
+    console.log(searchResult)
+    console.log(searchResult.length)
+    if (searchResult.length < 10 && searchResult.length > 1) {
+        console.log("1")
+        return (
+            <>
+                {countries.filter((element) => element.name.common.includes(search)).map((country) => 
+                    <p key={country.ccn3} >{country.name.common}</p>
+                )}
+            </>
+        )
+    } else if (searchResult.length > 10) {
+        console.log("2")
+        return (
+            <p>Too Many Matches, specify another filter</p>
+        )
+    } else if (searchResult.length === 1) {
+        console.log("3")
+        console.log(searchResult[0].name.common)
+        return (
+            <>
+                <h1>{searchResult[0].name.common}</h1>
+                <br/>
+                <p>capital: {searchResult[0].capital}</p>
+                <p>area: {searchResult[0].area}</p>
+                <br/>
+                <h2>Languages</h2>
+                <ul>
+                    {Object.entries(searchResult[0].languages).map(item => {
+                        console.log(item)
+                        return (
+                            <li>{item[1]}</li>
+                        )
+                    })}
+                </ul>
+                <br/>
+                <img src={searchResult[0].flags.png} alt="flag"/>
+            </>
+        )
+    }
+}
+
+const App = () => {
+    const [countries, setCountries] = useState([])
+    const [search, setSearch] = useState('')
+    useEffect(() => {
+        axios
+          .get('https://restcountries.com/v3.1/all')
+          .then(response => {
+            console.log('promise fulfilled')
+            setCountries(response.data)
+          })
+      }, [])
+
+
+    const searchType = e => {      
+        console.log(e.target.value)
+        console.log(countries.filter(element => element.name.common.includes(search)))
+        console.log(countries.filter(element => element.name.common.includes(search)).length)
+        return (
+            setSearch(e.target.value)
+        )
+    }
+
+    return (
+        <>
+            <Filter search={search} handleSearch={searchType} />
+            <Result countries={countries} search={search} />
+        </>
+    )
+}
+
+export default App;
--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter.js'
 import PersonForm from './components/PersonForm.js'
 import Persons from './components/Persons.js'
@@ -14,7 +15,17 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search1, setSearch1] = useState('')
-  
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNewName(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
   const addPerson = (e) => {
     e.preventDefault()

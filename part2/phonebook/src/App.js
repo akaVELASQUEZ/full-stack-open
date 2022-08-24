16e8596607d39e35940ca23cfd36b98345714bcc
akaVELASQUEZ <allanvelasquez2397@gmail.com>
2022-08-24 11:13:15 +0800

    Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,12 +5,7 @@ import PersonForm from './components/PersonForm.js'
 import Persons from './components/Persons.js'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -22,7 +17,7 @@ const App = () => {
       .get('http://localhost:3001/persons')
       .then(response => {
         console.log('promise fulfilled')
-        setNewName(response.data)
+        setPersons(response.data)
       })
   }, [])
   console.log('render', persons.length, 'persons')
